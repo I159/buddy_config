69971ec7b885278a2b915661edd855b97a15615d
Ilya Pekelny <pekelny@gmail.com>
2019-06-23 17:38:11 +0300

    Allow default different values on instances.

--- config.py
+++ config.py
@@ -5,13 +5,16 @@ from collections import namedtuple
 
 
 class Setting:
-    def __init__(self, name, default=None, cast_type=None):
+    name = None
+    cast_type = None
+
+    def __init__(self, name, cast_type=None):
         self.name = name
-        self.default = default
-        if inspect.isclass(cast_type):
-            self.cast_type = cast_type
-        else:
-            raise TypeError(f"{cast_type} is not a class.")
+        if cast_type:
+            if inspect.isclass(cast_type):
+                self.cast_type = cast_type
+            else:
+                raise TypeError(f"{cast_type} is not a class.")
 
 
 class ConfigurationError(Exception):
@@ -22,7 +25,14 @@ class ConfigurationError(Exception):
         )
 
 
+class ConfigDefautsPasser:
+    def __init__(self, **default_settings):
+        self._defaults = default_settings
+
+
 class Config:
+    _defaults = {}
+
     @staticmethod
     def _ensure_env(setting_func):
         @functools.wraps(setting_func)
@@ -52,22 +62,37 @@ class Config:
     def __new__(cls, class_name, bases, attrs):
         for name in attrs:
             if name.isupper():
-                setting = Setting(*attrs[name])
+                # FIXME: save setting to a dict and then operate on separate Setting instances.
+                setting = (
+                    Setting(*attrs[name])
+                    if isinstance(attrs[name], tuple)
+                    else Setting(attrs[name])
+                )
 
-                if setting.default:
-
-                    def _setting_getter(self):
-                        return os.environ.get(setting.name, setting.default)
-
-                else:
-
-                    def _setting_getter(self):
-                        return os.environ[setting.name]
+                def _setting_getter(self):
+                    # FIXME: save setting to a dict and then operate on separate Setting instances.
+                    if setting.name in self._defaults:
+                        return os.environ.get(
+                            setting.name, self._defaults[setting.name]
+                        )
+                    return os.environ[setting.name]
 
                 _setting_getter.__name__ = name
                 _setting_getter.var_name = setting
                 attrs[name] = cls._ensure_env(_setting_getter)
                 if setting.cast_type:
                     attrs[name] = cls._cast_as(setting.cast_type)(attrs[name])
+                # attrs[name] = property(attrs[name])
+                bases = (ConfigDefautsPasser,)
 
         return type(class_name, bases, attrs)
+
+
+if __name__ == "__main__":
+
+    class MyConf(metaclass=Config):
+        AAA = "AAAa"
+        BBB = "BBBb", int
+
+    my_conf = MyConf(AAA=2)
+    my_conf.AAA()
