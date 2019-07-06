a4c89286a6ab9e9a477fa5ada8e05d19e1f13e11
Ilya Pekelny <pekelny@gmail.com>
2019-07-06 16:55:08 +0300

    Required data type for a setting

--- config.py
+++ config.py
@@ -2,17 +2,17 @@
 
 Buddy-Config allows to use environment variables in lazy and flexible way. Load
 a config settings exactly when they are needed but not at import. Define non-global
-defaults. Default values could be different for the same config in each its instance.
+defaults. Default values could be different for the same config in each of its instance.
 Use declarative style.
 
 Examples:
 
     >>> class MyConf(metaclass=Config):
-    >>>     NAME_OF_A_SETTING_A = "NAME_OF_AN_ENVIRONMENT_VARIABLE_A"
+    >>>     NAME_OF_A_SETTING_A = "NAME_OF_AN_ENVIRONMENT_VARIABLE_A", str
     >>>     NAME_OF_A_SETTING_C = "NAME_OF_AN_ENVIRONMENT_VARIABLE_C", int, lambda x: x ** 2
     >>>     NAME_OF_A_SETTING_B = "NAME_OF_AN_ENVIRONMENT_VARIABLE_B", int
     >>>     COMBINED_SETTING = (
-    >>>         lambda self: self.NAME_OF_A_SETTING_A + self.NAME_OF_A_SETTING_B
+    >>>         lambda self: int(self.NAME_OF_A_SETTING_A) + self.NAME_OF_A_SETTING_B
     >>>     )
 
     >>> my_conf = MyConf(NAME_OF_A_SETTING_A=2)
@@ -28,7 +28,6 @@ Examples:
     >>> print(my_conf.COMBINED_SETTING)
 """
 import functools
-import inspect
 import os
 
 
@@ -43,6 +42,8 @@ class ConfigurationError(Exception):
 
 
 class ConfigDefautsPasser:
+    """Base class to add defaults for the settings."""
+
     def __init__(self, **default_settings):
         self._defaults = default_settings
 
@@ -52,6 +53,8 @@ class Config:
 
     @staticmethod
     def _ensure_env(setting_func):
+        """Check if the variable is set in the environment."""
+
         @functools.wraps(setting_func)
         def wrapper(self):
             try:
@@ -69,9 +72,12 @@ class Config:
 
     @staticmethod
     def _apply_processor(processor):
+        """Apply a custom setting value processor."""
         if not callable(processor):
             raise TypeError(
-                f"Processor should be a callable — a class or a function: {processor}"
+                "Processor should be a callable — a class or a function: {}|{}".format(
+                    type(processor), processor
+                )
             )
 
         def _apply(setting_func):
@@ -83,8 +89,44 @@ class Config:
 
         return _apply
 
+    @staticmethod
+    def _apply_type(type_):
+        """Cast a setting to a type."""
+        if not issubclass(type_, (int, float, str, bytes)):
+            raise TypeError(
+                "Cast type must be a pure data type: int, float, str or bytes."
+                "Actual type is: {}".format(type(type_))
+            )
+
+        def _apply(setting_func):
+            @functools.wraps(setting_func)
+            def wrapper(self):
+                return type_(setting_func(self))
+
+            return wrapper
+
+        return _apply
+
+    @staticmethod
+    def _ensure_type(setting_func):
+        """Ensure that a setting after custom processing still of pure data types."""
+
+        @functools.wraps(setting_func)
+        def wrapper(self):
+            res = setting_func(self)
+            if not isinstance(res, (int, float, str, bytes)):
+                raise TypeError(
+                    "Custom setting processors must return pure "
+                    "data types: int, float, str or bytes"
+                )
+            return res
+
+        return wrapper
+
     @classmethod
     def _make_setting_prop(cls, setting_name, var_name):
+        """Create a setting property"""
+
         def _setting_getter(self):
             if setting_name in self._defaults:
                 return os.environ.get(var_name, self._defaults[setting_name])
@@ -96,22 +138,26 @@ class Config:
         return _setting_getter
 
     def __new__(cls, class_name, bases, attrs):
-        for setting_name, v in attrs.items():
+        """Create a config class from a declarative definition."""
+        for setting_name, setting in attrs.items():
             if setting_name.isupper():
-                if isinstance(v, tuple):
-                    var_name = v[0]
-                    pre_processors = v[1:]
-                elif isinstance(v, str):
-                    var_name = v
-                    pre_processors = None
+                if not callable(setting):
+                    setting = list(setting)
+                    var_name = setting.pop(0)
+                    setting_type = setting.pop(0)
+                    pre_processors = setting if setting else None
 
-                if not callable(v):
                     _setting_getter = cls._make_setting_prop(setting_name, var_name)
                     if pre_processors:
                         for i in pre_processors:
+                            _setting_getter = cls._apply_type(setting_type)(
+                                _setting_getter
+                            )
                             _setting_getter = cls._apply_processor(i)(_setting_getter)
+                    _setting_getter = cls._apply_type(setting_type)(_setting_getter)
                 else:
-                    _setting_getter = v
+                    _setting_getter = cls._ensure_type(setting)
+
                 _setting_getter = property(_setting_getter)
                 attrs[setting_name] = _setting_getter
                 bases = (ConfigDefautsPasser,)
